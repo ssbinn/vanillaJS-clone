d74c6278078eb42e7d1286fb299c001a65338494
최수빈 <83692497+ssbinn@users.noreply.github.com>
2022-04-21 10:59:15 +0900

    Update and rename index.md to index.js

---
+++ challenge/day8-9/index.js
@@ -0,0 +1,53 @@
+// maxNum.value, userNum.value가 중복되니 변수로 만들어 관리
+// 하나의 이벤트 핸들러 함수 안에 예외처리 if문을 두는 것 vs 모두 함수로 분리시켜 call 하는 것 중 좋은 코드는 뭘까 
+  
+  
+
+const maxNum = document.querySelector("#max-number input");
+const userNum = document.querySelector("#guess-number input");
+const playBtn = document.querySelector("#guess-number button");
+
+const answer = document.querySelector("#answer");
+const choose = document.querySelector("#answer #choose");
+const winOrLose = document.querySelector("#answer #win-lose");
+
+const getMachineNum = localStorage.getItem("machineN");
+const getUserNum = localStorage.getItem("userN");
+
+function handleSubmit(event) {
+  event.preventDefault(); // 새로고침 방지
+
+  // 입력 값이 없는 경우
+  if (maxNum.value === "" || userNum.value === "") {
+    alert("값을 입력하세요.");
+    return false;
+  }
+
+  // 입력 범위에 음수가 포함되는 경우
+  if (maxNum.value < 0 || userNum.value < 0) {
+    alert("입력한 숫자 범위에 음수가 포함되어 절대값을 반환하도록 하겠습니다.");
+    maxNum.value = Math.abs(maxNum.value);
+    userNum.value = Math.abs(userNum.value);
+  }
+
+  // 조건: 0 <= machineNum <= maxNum
+  const machineNum = Math.floor(
+    Math.random() * (parseInt(maxNum.value, 10) + 1)
+  ); // if maxNum = 40, 0 <= machineNum <= 40
+  localStorage.setItem("machineN", machineNum);
+  localStorage.setItem("userN", userNum.value);
+
+  if (getMachineNum != null && getUserNum != null) {
+    answer.classList.remove("hidden");
+  }
+  choose.innerText = `You chose: ${userNum.value}, the machine chose: ${machineNum}.`;
+  // console.log(typeof machineNum); // number
+  // console.log(typeof userNum.value); //string
+  if (machineNum === parseInt(userNum.value, 10)) {
+    winOrLose.innerText = "You won!";
+  } else {
+    winOrLose.innerText = "You lost!";
+  }
+}
+
+playBtn.addEventListener("click", handleSubmit);
